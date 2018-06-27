03116b1d674c146c900ab68159b2a4772c108ca0
Maher Santina <maher.santina90@gmail.com>
2018-06-27 20:05:23 +1000

    Added support for vertical scroll

--- MSPeekCollectionViewDelegateImplementation/Classes/MSPeekCollectionViewDelegateImplementation.swift
+++ MSPeekCollectionViewDelegateImplementation/Classes/MSPeekCollectionViewDelegateImplementation.swift
@@ -26,8 +26,6 @@ extension UICollectionView {
         self.decelerationRate = UIScrollViewDecelerationRateFast
         self.showsHorizontalScrollIndicator = false
         self.showsVerticalScrollIndicator = false
-        let layout = self.collectionViewLayout as! UICollectionViewFlowLayout
-        layout.scrollDirection = .horizontal
     }
 }
 
@@ -38,13 +36,15 @@ open class MSPeekCollectionViewDelegateImplementation: NSObject, UICollectionVie
     private let scrollThreshold: CGFloat
     private let maximumItemsToScroll: Int
     private let numberOfItemsToShow: Int
+    private let scrollDirection: UICollectionViewScrollDirection
     
     private var currentScrollOffset: CGPoint = CGPoint(x: 0, y: 0)
     
     private lazy var itemWidth: (UIView) -> CGFloat = {
         view in
+        var frameWidth: CGFloat = self.getViewWidth(view)
         //Get the total remaining width for the
-        let itemsWidth = (view.frame.size.width
+        let itemsWidth = (frameWidth
                 //If we have 2 items, there will be 3 spacing and so on
                 - (CGFloat(self.numberOfItemsToShow + 1) * (self.cellSpacing))
                 //There's always 2 peeking cells even if there are multiple cells showing
@@ -59,30 +59,79 @@ open class MSPeekCollectionViewDelegateImplementation: NSObject, UICollectionVie
         guard self.itemWidth(view) > 0 else {
             return 0
         }
-        return Int(round(self.currentScrollOffset.x/self.itemWidth(view)))
+        let offset = self.getValueFromPoint(self.currentScrollOffset)
+        return Int(round(offset/self.itemWidth(view)))
     }
     
-    public init(cellSpacing: CGFloat = 20, cellPeekWidth: CGFloat = 20, scrollThreshold: CGFloat = 50, maximumItemsToScroll: Int = 1, numberOfItemsToShow: Int = 1) {
+    private func getViewWidth(_ view: UIView) -> CGFloat {
+        switch scrollDirection {
+        case .horizontal:
+            return view.frame.size.width
+        case .vertical:
+            return view.frame.size.height
+        }
+    }
+    
+    private func getValueFromPoint(_ point: CGPoint) -> CGFloat {
+        switch scrollDirection {
+        case .horizontal:
+            return point.x
+        case .vertical:
+            return point.y
+        }
+    }
+    
+    private func getPointFromValue(_ value: CGFloat, defaultPoint: CGPoint) -> CGPoint {
+        switch scrollDirection {
+        case .horizontal:
+            return CGPoint(x: value, y: defaultPoint.y)
+        case .vertical:
+            return CGPoint(x: defaultPoint.x, y: value)
+        }
+    }
+    
+    private func getSizeFromValue(_ value: CGFloat, defaultSize: CGSize) -> CGSize {
+        switch scrollDirection {
+        case .horizontal:
+            return CGSize(width: value, height: defaultSize.height)
+        case .vertical:
+            return CGSize(width: defaultSize.width, height: value)
+        }
+    }
+    
+    private func getEdgeInsets() -> UIEdgeInsets {
+        let insets = cellSpacing + cellPeekWidth
+        switch scrollDirection {
+        case .horizontal:
+            return UIEdgeInsets(top: 0, left: insets, bottom: 0, right: insets)
+        case .vertical:
+            return UIEdgeInsets(top: insets, left: 0, bottom: insets, right: 0)
+        }
+    }
+    
+    public init(cellSpacing: CGFloat = 20, cellPeekWidth: CGFloat = 20, scrollThreshold: CGFloat = 50, maximumItemsToScroll: Int = 1, numberOfItemsToShow: Int = 1, scrollDirection: UICollectionViewScrollDirection = .horizontal) {
         self.cellSpacing = cellSpacing
         self.cellPeekWidth = cellPeekWidth
         self.scrollThreshold = scrollThreshold
         self.maximumItemsToScroll = maximumItemsToScroll
         self.numberOfItemsToShow = numberOfItemsToShow
+        self.scrollDirection = scrollDirection
     }
     
     open func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        guard scrollView.frame.size.width > 0 else {
+        let viewWidth = getViewWidth(scrollView)
+        guard viewWidth > 0 else {
             return
         }
         let target = targetContentOffset.pointee
         //Current scroll distance is the distance between where the user tapped and the destination for the scrolling (If the velocity is high, this might be of big magnitude)
-        let currentScrollDistance = target.x - currentScrollOffset.x
+        let currentScrollDistance = getValueFromPoint(target) - getValueFromPoint(currentScrollOffset)
         let coefficient = calculateCoefficient(scrollDistance: currentScrollDistance, scrollWidth: itemWidth(scrollView))
         
         let adjacentItemIndex = currentItemIndex(scrollView) + coefficient
         let adjacentItemOffsetX = self.scrollView(scrollView, contentOffsetForItemAtIndex: adjacentItemIndex)
         
-        targetContentOffset.pointee = CGPoint(x: adjacentItemOffsetX, y: target.y)
+        targetContentOffset.pointee = getPointFromValue(adjacentItemOffsetX, defaultPoint: target)
     }
     
     open func scrollView(_ scrollView: UIScrollView, contentOffsetForItemAtIndex index: Int) -> CGFloat{
@@ -105,12 +154,11 @@ open class MSPeekCollectionViewDelegateImplementation: NSObject, UICollectionVie
     }
     
     open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: itemWidth(collectionView), height: collectionView.frame.size.height)
+        return getSizeFromValue(itemWidth(collectionView), defaultSize: collectionView.frame.size)
     }
     
     open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        let leftAndRightInsets = cellSpacing + cellPeekWidth
-        return UIEdgeInsets(top: 0, left: leftAndRightInsets, bottom: 0, right: leftAndRightInsets)
+        return getEdgeInsets()
     }
     
     open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
