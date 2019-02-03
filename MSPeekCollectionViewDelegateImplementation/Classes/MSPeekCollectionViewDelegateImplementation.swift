a8c57e65125d579e59fd78508722529f55aac533
Maher Santina <maher.santina90@gmail.com>
2019-02-03 18:52:29 +1100

    Moved helper functions to scroll direction enum

--- MSPeekCollectionViewDelegateImplementation/Classes/MSPeekCollectionViewDelegateImplementation.swift
+++ MSPeekCollectionViewDelegateImplementation/Classes/MSPeekCollectionViewDelegateImplementation.swift
@@ -108,7 +108,7 @@ open class MSPeekCollectionViewDelegateImplementation: NSObject {
     
     fileprivate lazy var itemLength: (UIView) -> CGFloat = {
         view in
-        var frameWidth: CGFloat = self.getViewLength(view)
+        var frameWidth: CGFloat = self.scrollDirection.length(for: view)
         //Get the total remaining width for the
         let allItemsWidth = (frameWidth
                 //If we have 2 items, there will be 3 spacing and so on
@@ -120,31 +120,6 @@ open class MSPeekCollectionViewDelegateImplementation: NSObject {
         return max(0, finalWidth)
     }
     
-    fileprivate func getViewLength(_ view: UIView) -> CGFloat {
-        return scrollDirection.length(for: view)
-    }
-    
-    fileprivate func getValueFromPoint(_ point: CGPoint) -> CGFloat {
-        return scrollDirection.value(for: point)
-    }
-    
-    fileprivate func getValueFromSize(_ size: CGSize) -> CGFloat {
-        return scrollDirection.value(for: size)
-    }
-    
-    fileprivate func getPointFromValue(_ value: CGFloat, defaultPoint: CGPoint) -> CGPoint {
-        return scrollDirection.point(for: value, defaultPoint: defaultPoint)
-    }
-    
-    fileprivate func getSizeFromValue(_ value: CGFloat, defaultSize: CGSize) -> CGSize {
-        return scrollDirection.size(for: value, defaultSize: defaultSize)
-    }
-    
-    fileprivate func getEdgeInsets() -> UIEdgeInsets {
-        let insets = cellSpacing + cellPeekWidth
-        return scrollDirection.edgeInsets(for: insets)
-    }
-    
     public init(cellSpacing: CGFloat = 20, cellPeekWidth: CGFloat = 20, scrollThreshold: CGFloat = 50, maximumItemsToScroll: Int = 1, numberOfItemsToShow: Int = 1, scrollDirection: UICollectionViewScrollDirection = .horizontal) {
         self.cellSpacing = cellSpacing
         self.cellPeekWidth = cellPeekWidth
@@ -154,34 +129,12 @@ open class MSPeekCollectionViewDelegateImplementation: NSObject {
         self.scrollDirection = scrollDirection
     }
     
-    open func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        let viewWidth = getViewLength(scrollView)
-        guard viewWidth > 0 else {
-            return
-        }
-        let target = targetContentOffset.pointee
-        //Current scroll distance is the distance between where the user tapped and the destination for the scrolling (If the velocity is high, this might be of big magnitude)
-        let currentScrollDistance = getValueFromPoint(target) - getValueFromPoint(currentScrollOffset)
-        let coefficient = calculateCoefficient(scrollDistance: currentScrollDistance, scrollWidth: itemLength(scrollView))
-        
-        let adjacentItemNumber = self.scrollView(scrollView, indexForItemAtContentOffset: currentScrollOffset) + coefficient
-        let adjacentItemOffsetX = self.scrollView(scrollView, contentOffsetForItemAtIndex: adjacentItemNumber)
-        
-        targetContentOffset.pointee = getPointFromValue(adjacentItemOffsetX, defaultPoint: target)
-        
-        //Get the new active index
-        let activeIndex = self.scrollView(scrollView, indexForItemAtContentOffset: targetContentOffset.pointee)
-        //Pass the active index to the delegate
-        delegate?.scrollViewWillEndDragging?(scrollView, withVelocity: velocity, targetContentOffset: targetContentOffset)
-        delegate?.peekImplementation(self, didChangeActiveIndexTo: activeIndex)
-    }
-    
     open func scrollView(_ scrollView: UIScrollView, indexForItemAtContentOffset contentOffset: CGPoint) -> Int {
         let width = itemLength(scrollView) + cellSpacing
         guard width > 0 else {
             return 0
         }
-        let offset = self.getValueFromPoint(contentOffset)
+        let offset = self.scrollDirection.value(for: contentOffset)
         let index = Int(round(offset/width))
         return index
     }
@@ -190,7 +143,7 @@ open class MSPeekCollectionViewDelegateImplementation: NSObject {
         return CGFloat(index) * (itemLength(scrollView) + cellSpacing)
     }
     
-    private func calculateCoefficient(scrollDistance: CGFloat, scrollWidth: CGFloat) -> Int {
+    fileprivate func calculateCoefficient(scrollDistance: CGFloat, scrollWidth: CGFloat) -> Int {
         var coefficent = 0
         let safeScrollThreshold = max(scrollThreshold, 0.1)
         if abs(scrollDistance/safeScrollThreshold) <= 1 {
@@ -213,20 +166,43 @@ extension MSPeekCollectionViewDelegateImplementation: UICollectionViewDelegateFl
         delegate?.scrollViewWillBeginDragging?(scrollView)
     }
     
-    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return getSizeFromValue(itemLength(collectionView), defaultSize: collectionView.frame.size)
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return scrollDirection.size(for: itemLength(collectionView), defaultSize: collectionView.frame.size)
     }
     
-    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return getEdgeInsets()
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let insets = cellSpacing + cellPeekWidth
+        return scrollDirection.edgeInsets(for: insets)
     }
     
-    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
     
-    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return cellSpacing
+    }
+    
+    public func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let viewWidth = scrollDirection.length(for: scrollView)
+        guard viewWidth > 0 else {
+            return
+        }
+        let target = targetContentOffset.pointee
+        //Current scroll distance is the distance between where the user tapped and the destination for the scrolling (If the velocity is high, this might be of big magnitude)
+        let currentScrollDistance = scrollDirection.value(for: target) - scrollDirection.value(for: currentScrollOffset)
+        let coefficient = calculateCoefficient(scrollDistance: currentScrollDistance, scrollWidth: itemLength(scrollView))
+        
+        let adjacentItemNumber = self.scrollView(scrollView, indexForItemAtContentOffset: currentScrollOffset) + coefficient
+        let adjacentItemOffsetX = self.scrollView(scrollView, contentOffsetForItemAtIndex: adjacentItemNumber)
+        
+        targetContentOffset.pointee = scrollDirection.point(for: adjacentItemOffsetX, defaultPoint: target)
+        
+        //Get the new active index
+        let activeIndex = self.scrollView(scrollView, indexForItemAtContentOffset: targetContentOffset.pointee)
+        //Pass the active index to the delegate
+        delegate?.scrollViewWillEndDragging?(scrollView, withVelocity: velocity, targetContentOffset: targetContentOffset)
+        delegate?.peekImplementation(self, didChangeActiveIndexTo: activeIndex)
     }
     
     
